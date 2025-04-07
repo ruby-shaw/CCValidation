2e87e3d2f86779b84ec5a8442fe70279d36a50e4
ruby.shaw <ruby.shaw@MU-C1-17.mullen.pvt.k12.co.us>
2025-04-07 10:35:05 -0600

    Initial commit

---
+++ CCValidation/src/CCValidation.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
+public class CCValidation 
+{
+	public static int CCNumbers[] = new int [99];
+	
+	public static void main(String[] args) 
+	{
+		
+
+	}
+	
+	public static void getCCNumbers() throws IOException
+	{
+		Scanner myFile = new Scanner(new File("CCnumbers.txt"));
+		
+		while(myFile.hasNext())
+		{
+//			CCNumbers{
+		}
+	}
+	
+	public static int doubleNumbers()
+	{
+		int doubles = 0; 
+		return doubles;
+	}
+	// 
+
+}
