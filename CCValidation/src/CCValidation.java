faaab9a4e91a4f640b77653ddc81bd5574879265
amyshaw <amyshaw@amys-mbp.hsd1.co.comcast.net>
2025-04-22 13:10:11 -0600

    Fixedddd

--- CCValidation/src/CCValidation.java
+++ CCValidation/src/CCValidation.java
@@ -13,42 +13,52 @@ public class CCValidation
 	public static void main(String[] args) 
 	{
 
-//		Scanner user = new Scanner(System.in);
-//		System.out.println("Please enter a 16 digit number");
-//		String cardNumber = user.nextLine(); 
-//		number = cardNumber; 
+		Scanner user = new Scanner(System.in);
+		System.out.println("Please enter a 16 digit number");
+		String cardNumber = user.nextLine(); 
+		number = cardNumber; 
+		
+		isValid(number);
+		if (isValid(number)== true)
+		{
+			System.out.println("That number is valid!");
+		}
+		else
+		{
+			System.out.println("That number is invalid!");
+		}
 		
 		
-		try 
-		{
-        File file = new File("CCnumbers");
-        Scanner scanner = new Scanner(file);
-
-        int lineNum = 1;
-        while (scanner.hasNextLine()) 
-        {
-            String number = scanner.nextLine().trim();
-
-            if (number.length() != 16 || !number.matches("\\d{16}")) 
-            {
-                System.out.println("Line " + lineNum + ": INVALID INPUT ");
-            } 
-            else 
-            {
-                boolean isValid = isValid(number);
-                System.out.println("Line " + lineNum + ": " + (isValid ? "VALID" : "INVALID"));
-            }
-            lineNum++;
-        }
-
-        scanner.close();
-		}
-		catch (FileNotFoundException e) 
-		{
-        System.out.println("file not found. Check the name and path.");
-        
-        e.printStackTrace();
-		}
+//		try 
+//		{
+//        File file = new File("CCnumbers");
+//        Scanner scanner = new Scanner(file);
+//
+//        int lineNum = 1;
+//        while (scanner.hasNextLine()) 
+//        {
+//            String number = scanner.nextLine().trim();
+//
+//            if (number.length() != 16 || !number.matches("\\d{16}")) 
+//            {
+//                System.out.println("Line " + lineNum + ": INVALID INPUT ");
+//            } 
+//            else 
+//            {
+//                boolean isValid = isValid(number);
+//                System.out.println("Line " + lineNum + ": " + (isValid ? "VALID" : "INVALID"));
+//            }
+//            lineNum++;
+//        }
+//
+//        scanner.close();
+//		}
+//		catch (FileNotFoundException e) 
+//		{
+//        System.out.println("file not found. Check the name and path.");
+//        
+//        e.printStackTrace();
+//		}
 		
 		 
 		
